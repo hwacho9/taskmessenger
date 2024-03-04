c4f0a84fc1b494892489447b2013e2e7a7f854d4
hwacho9 <63436365+hwacho9@users.noreply.github.com>
2024-03-04 17:56:05 +0900

    first settings

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Profile from "./routes/profile";
+import Home from "./routes/home";
+import Login from "./routes/login";
+import CreateAccount from "./routes/create-account";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+import { auth } from "./firebase";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { path: "", element: <Home /> },
+            { path: "profile", element: <Profile /> },
+        ],
+    },
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/create-account",
+        element: <CreateAccount />,
+    },
+]);
+
+function App() {
+    const [isLoading, setIsLoading] = useState(true);
+    const init = async () => {
+        await auth.authStateReady();
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    return (
+        <>
+            {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+        </>
+    );
+}
+
+export default App;
---
+++ src/firebase.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD_aBz6Bva8QgKPGkh6qrCMuHpun9o0ldc",
+    authDomain: "taskmessenger-4667d.firebaseapp.com",
+    projectId: "taskmessenger-4667d",
+    storageBucket: "taskmessenger-4667d.appspot.com",
+    messagingSenderId: "371404961854",
+    appId: "1:371404961854:web:82748090cbb8327f4cf96f",
+    measurementId: "G-MM6HG66GHB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
