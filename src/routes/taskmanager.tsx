2d6e4cd834431d86a874616e3f347f24f7e52c89
ImagawaKosuke <kosuke8de@gmail.com>
2024-03-07 14:51:14 +0900

    スプシをHTMLに出している最中

--- src/routes/taskmanager.tsx
+++ src/routes/taskmanager.tsx
@@ -1,17 +1,19 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet';
+//スプシの内容を反映
+const apiURL = 'https://script.google.com/macros/s/AKfycbzzKlMepkunLhD-QVP5vhF6xUwNIXpRzUFq6n-zgKHFBT-viZ9YE8nZO07I8BdNhfyOmQ/exec';
+const data = fetch(apiURL)
+    .then(function (fetch_data) {
+        return fetch_data.json();
+    })
+    .then(function (json) {
+        for (var i in json) {
+            console.log(json[i].date);
+        }
+    });
+
 export default function Profile() {
-    return <h1>taskmanager</h1>;
+    const response = fetch(apiURL);
+    console.log(response)
+    return (<div>
+        <h1>taskmanager</h1>
+    </div>);
 }
-const SHEET_ID = '1aPUSsI8m5vCey5PXyVMwNV_wsKXqfyA2E8cHzQf8UX4';
-async function getSheetInfo() {
-    const doc = new GoogleSpreadsheet(SHEET_ID);
-    await doc.loadInfo();
-    // シート名
-    const sheetTitle = doc.sheetsById[0].title;
-    // セルの値
-    const cellValue = doc.sheetsById[0].getCellByA1('A1').value;
-    console.log(`シート名: ${sheetTitle}`);
-    console.log(`セルの値: ${cellValue}`);
-}
-getSheetInfo();
-console.log("taskmanager");
