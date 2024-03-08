ac4b00f01b196fe16d269f8c77b4671e10cfe32e
ImagawaKosuke <kosuke8de@gmail.com>
2024-03-08 14:44:00 +0900

    文字列処理完了

--- src/routes/taskmanager.tsx
+++ src/routes/taskmanager.tsx
@@ -1,35 +1,66 @@
 import { fetchTasks } from "../components/tasks"
 import { Chat } from "../components/Gpt-result"
+
 import { useEffect, useState } from "react";
 
+//タスク抽出する関数
+function process(text: string) {
+    var tex1 = ""
+    var tex2 = ""
+    const todos = [];
+    for (var i = 1; i < 7; i++) {
+        tex1 = `${text.split(i.toString() + ".")[1]}`;
+        tex2 = `${tex1.split((i + 1).toString() + ".")[0]}`;
+        todos.push(tex2)
+    }
+    tex1 = `${text.split("7.")[1]}`;
+    todos.push(tex1)
+    const tasks = [];
+    var todotask = "";
+    var priority = "";
+    var from = "";
+    var due_date = "";
+    for (const item of todos) {
+        todotask = `${item.split("task: ")[1]}`;
+        priority = `${item.split("priority: ")[1]}`;
+        from = `${item.split("from: ")[1]}`;
+        due_date = `${item.split("due_date: ")[1]}`;
+        tasks.push({
+            task: `${todotask.split("\n")[0]}`,
+            priority: `${priority.split("\n")[0]}`,
+            from: `${from.split("\n")[0]}`,
+            due_date: `${due_date.split("\n")[0]}`,
+        });
+    }
+    console.log(tasks);
+    return tasks
+}
 
 export default function Profile() {
     const [tasks, setTasks] = useState<any[]>([]); //anyがたに合わせるようにした
     const [gptResult, setGptResult] = useState<string>(""); // 新しい状態変数
 
-
     useEffect(() => {
         fetchTasks().then((data) => setTasks(data));
-        Chat().then((result) => setGptResult(result));
+        Chat().then((result) => setGptResult(result)); //GPT使うときに使用
     }, []);
+    var Tex = process(gptResult);
+    console.log(Tex);
     //内容をコピペ
     return (
         <div>
             <h1>taskmanager</h1>
             <ul>
-                {tasks.map((task) => (
-                    <li key={task.id}>
-                        <div>{task.date}</div>
-                        <div>From: {task.from}</div>
-                        <div>Content: {task.content}</div>
+                <div>GPT-3による抽出結果</div>
+                {Tex.map((task) => (
+                    <li>
+                        <div>task: {task.task}</div>
+                        <div>priority: {task.priority}</div>
+                        <div>from: {task.from}</div>
+                        <div>due_date: {task.due_date}</div>
                     </li>
                 ))}
-                {gptResult && (
-                    <li>
-                        <div>GPT-3による抽出結果</div>
-                        <div>{gptResult}</div>
-                    </li>
-                )}
+
             </ul>
         </div>
     );
